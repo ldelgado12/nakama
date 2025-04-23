d8a62dac72547accc8fc786ed485daa05d7f9e7b
ldelgado12 <50750108+ldelgado12@users.noreply.github.com>
2025-04-23 12:05:13 -0600

    primer commit
    icono ingreso, carrtio, header y footer

---
+++ nakama/lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NakamaStore';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Pagina de ventas en linea de la tienda Nakama Store';
+
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
