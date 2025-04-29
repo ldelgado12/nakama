8aad33aaf2c77ae51ed0004e7e5a42fd60620a98
ldelgado12 <50750108+ldelgado12@users.noreply.github.com>
2025-04-28 19:27:32 -0600

    solucion de errores
    agregando la solucion de los errores en el loading, not found, menu, index

--- nakama/components/shared/header/index.tsx
+++ nakama/components/shared/header/index.tsx
@@ -1,42 +1,31 @@
-import { ShoppingCartIcon, UserIcon } from 'lucide-react';
+
 import Image from 'next/image';
 import Link from 'next/link';
-import { Button } from '@/components/ui/button';
 import { APP_NAME } from '@/lib/constants';
-import ModeToggle from './mode-toggle';
+import Menu from './menu';
+
 
 const Header = () => {
-    return  <header className='w-full border-b'>
+    return ( 
+    <header className='w-full border-b'>
         <div className="wrapper flex-between">
             <div className="flex-start">
                 <Link href='/' className='flex-start'>
-                <Image 
-                src='/images/logo.svg' 
-                alt={`${APP_NAME} logo`}
-                height={48} 
-                width={48} 
-                priority={true}
-                />
-                <span className="hidden lg:block font-bold text-2xl ml-3">
-                    {APP_NAME}
-                </span>
+                    <Image 
+                      src='/images/logo.svg' 
+                      alt={`${APP_NAME} logo`}
+                      height={48} 
+                      width={48} 
+                      priority={true}
+                    />
+                    <span className="hidden lg:block font-bold text-2xl ml-3">
+                        {APP_NAME}
+                    </span>
                 </Link>
             </div>
-            <div className="space-x-2">
-                <ModeToggle/>
-                <Button asChild variant='ghost'>
-                    <Link href='/cart'>
-                        <ShoppingCartIcon /> Carrito
-                    </Link>
-                </Button>
-                <Button asChild >
-                    <Link href='/Ingreso'>
-                        <UserIcon /> Ingreso
-                    </Link>
-                </Button>
-            </div>
+            <Menu/>
         </div>
-        </header>;
-};
+        </header>
+)};
  
 export default Header;
